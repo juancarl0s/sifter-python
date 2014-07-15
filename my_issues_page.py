507485fa65dc807b48f2e405b2049a50e21a204b
Ben <benspaul@gmail.com>
2014-07-14 21:28:02 -0400

    Update my_issues_page.py

--- my_issues_page.py
+++ my_issues_page.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 #
-#       sifter_print.py
+#       my_issues_page.py
 #       
 #       Copyright 2012 Mark Mikofski <marko@bwanamaro@yahoo.com>
 #       
